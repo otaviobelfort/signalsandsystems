7477f1bc0540d04fcd8384acc7e7f65f9b5ce8d3
otaviobelfort <otaviodeel@gmail.com>
2020-09-15 23:49:03 -0300

    Graph implementation

--- edo_ordem_01.py
+++ edo_ordem_01.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+"""
+'#!/usr/bin/env python3' aponta para o interpretador python e
+permite a execução no terminal com -> ./namefile.py
+"""
+"""
+    - Bibliografia: Boyce, W. E. and Diprima, R. C. (2012). Equações Diferenciais Elementares e
+Problemas de Valores de Contorno. LTC, 9 edition.
+   
+   Autor: Otávio Belfort
+   
+   github.com/otaviobelfort | facebook.com/otaviobelforth | youtube.com/user/canalwrkits
+"""  
+
 import matplotlib.pyplot
 import math
-
 #function generetor the vector
 def vector_generetor(i0,n,i1):
     vetor_t = list(range(i0, n, i1))
@@ -13,15 +25,11 @@ def vector_generetor(i0,n,i1):
 a = 0.5
 b = 450
 c = 1
-
-
 vector_result = []
 vector_time = vector_generetor(1,100,1)
 for element in vector_time:
     vector_result.append(b/a + (c * math.exp(a*element)))
-
-
 print(vector_time, vector_result)
 
 matplotlib.pyplot.plot(vector_time, vector_result)
-matplotlib.pyplot.show()
+matplotlib.pyplot.show() # plot graph
