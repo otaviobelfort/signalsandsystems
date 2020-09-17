b03c2fa759ad43201dfdfb5b532850e9c1e626dc
otaviobelfort <otaviodeel@gmail.com>
2020-09-16 23:54:41 -0300

    Update edo_ordem_01_versao2.py

--- edo_ordem_01_versao2.py
+++ edo_ordem_01_versao2.py
@@ -10,13 +10,12 @@ t = np.arange(0.0, 50, 0.1) # vector generetor the time
 '''
 a, b, = 0.5, 450 # constantes
 '''
-a = -0.2
-b = -9.8
+a, b= -0.2, -9.8
 
 equilib = b/a
-func1 = (equilib + 1) + np.exp(a*t) # (equilib + 1) -> um valor acima do ponto de equilibrio
+func1 = (equilib + 1) + np.exp(a*t) # (equilib + 1) -> a value above the breakeven point
 ponto_equilib = equilib + np.exp(a*t)*0
-func2 = (equilib - 1) - np.exp(a*t) # (equilib - 1) -> um valor abaixo do ponto de equilibrio
+func2 = (equilib - 1) - np.exp(a*t) # (equilib - 1) -> a value below the breakeven point
 
 plt.plot(t,func1,'r-', t,func2,'b-', t,ponto_equilib,'g-') 
 
