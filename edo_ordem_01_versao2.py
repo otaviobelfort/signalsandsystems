66d997a72d750d9f8bb5197c71b3579483777a89
otaviobelfort <otaviodeel@gmail.com>
2020-09-18 20:30:04 -0300

    Update edo_ordem_01_versao2.py

--- edo_ordem_01_versao2.py
+++ edo_ordem_01_versao2.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-a = 0.5
 t = np.arange(0.0, 50, 0.1) # vector generetor the time
 
 '''
@@ -20,24 +19,28 @@ a, b, = 0.5, 450 # constantes
 '''
 a, b= -0.2, -9.8
 
-equilib = b/a
-func1 = (equilib + 1) + np.exp(a*t) # (equilib + 1) -> a value above the breakeven point
-ponto_equilib = equilib + np.exp(a*t)*0
-func2 = (equilib - 1) - np.exp(a*t) # (equilib - 1) -> a value below the breakeven point
+def edo_ordem_01(a, b, t):
+    equilib = b/a
+    func1 = (equilib + 1) + np.exp(a*t) # (equilib + 1) -> a value above the breakeven point
+    ponto_equilib = equilib + np.exp(a*t)*0
+    func2 = (equilib - 1) - np.exp(a*t) # (equilib - 1) -> a value below the breakeven point
 
-plt.plot(t,func1,'r-', t,func2,'b-', t,ponto_equilib,'g-') 
+    plt.plot(t,func1,'r-', t,func2,'b-', t,ponto_equilib,'g-') 
 
-plt.xlabel('time (s)')
-plt.ylabel('(P(t))')
-'''
-plt.title('População de corujas - time plot')
-plt.grid(True)
-plt.savefig("População de corujas - time plot")
-# Graphic: https://github.com/otaviobelfort/signalsandsystems/blob/master/Popula%C3%A7%C3%A3o%20de%20corujas%20-%20time%20plot.png 
-'''
-plt.title('Sistemas Físico - time plot')
-plt.grid(True)
-plt.savefig("Sistemas Físico - time plot")
-# Graphic: https://github.com/otaviobelfort/signalsandsystems/blob/master/Sistemas%20F%C3%ADsico%20-%20time%20plot.png
+    plt.xlabel('time (s)')
+    plt.ylabel('(P(t))')
+    '''
+    plt.title('População de corujas - time plot')
+    plt.grid(True)
+    plt.savefig("População de corujas - time plot")
+    # Graphic: https://github.com/otaviobelfort/signalsandsystems/blob/master/Popula%C3%A7%C3%A3o%20de%20corujas%20-%20time%20plot.png 
+    '''
+    plt.title('Sistemas Físico - time plot')
+    plt.grid(True)
+    plt.savefig("Sistemas Físico - time plot")
+    # Graphic: https://github.com/otaviobelfort/signalsandsystems/blob/master/Sistemas%20F%C3%ADsico%20-%20time%20plot.png
 
-plt.show()
+    plt.show()
+
+#plot
+edo_ordem_01(-0.2, -9.8,t)
